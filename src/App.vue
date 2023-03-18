3d2d4b6803561698fe5bfb425777691c9da9a625
loveadewunmi <loveadewunmi19@gmail.com>
2023-03-18 22:52:56 +0100

    initexam

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<CounterView></CounterView>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CounterView from './components/CounterView.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {CounterView
+    
   }
 }
 </script>
---
+++ src/components/CounterView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>counter</div>
+    <div><input type="text" placeholder="setvalue" /></div>
+    <div>
+      <button>-</button>
+      <button>+</button>
+      <button>setvalue</button>
+      <button>reset</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
